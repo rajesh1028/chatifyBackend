4318425d803131f961db9ae0af15c7dacfe6ba3a
rajesh-fw22-0030 <rajeshkaruppusamy@gmail.com>
2023-04-06 17:29:53 +0530

    backend deployed

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 const express = require("express");
-const { connection } = require("./configs/db");
-const { statusRoute } = require("./routes/status.Route");
-require("dotenv").config();
 const cors = require("cors");
+
+const { connection } = require("./config/db");
+const { userRouter } = require("./routes/userRouter");
+const { passport } = require("./google-auth");
+const { statusRoute } = require("./routes/status.Route");
+
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Home Page...")
+app.get('/re', (ask, give) => {
+    let { profile } = require("./google-auth")
+    give.redirect(`http://127.0.0.1:5500/frontend/chatify.html?token=${profile.t}&name=${profile.name.givenName}&hfh=${profile.t}`);
 })
 
+app.get('/', (ask, give) => {
+    give.send('Welcome to Chatify.com Backend')
+})
+
+app.use('/user', userRouter);
 app.use("/status", statusRoute);
 
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/re' }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    });
 
 
-app.listen(process.env.port, async () => {
+
+app.listen(process.env.port, () => {
     try {
-        await connection;
-        console.log("connected to DB");
+        connection
+        console.log(`Connected to the DB and server is running at ${process.env.port}`)
     } catch (error) {
         console.log(error);
+        console.log("Error in connecting to the DB")
     }
-    console.log("server is listening at port " + process.env.port);
 })
